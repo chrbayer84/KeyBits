b73cac648dd7bedb7483f0c95d0e2459b113e05c
Christian Bayer <cbayer@demandware.com>
2015-04-04 23:06:10 +0200

    * fixed formatting

--- src/main/java/com/github/chrbayer84/keybits/Bases.java
+++ src/main/java/com/github/chrbayer84/keybits/Bases.java
@@ -1,40 +1,43 @@
 /**
- * Copyright (C) 2014 keybits@gmx.de This program is free software: you can redistribute it and/or modify it under the
- * terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the
- * License, or (at your option) any later version. This program is distributed in the hope that it will be useful, but
- * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See
- * the GNU General Public License for more details. You should have received a copy of the GNU General Public License
- * along with this program. If not, see https://www.gnu.org/licenses/gpl-3.0.txt.
+ *   Copyright (C) 2014
+ *   keybits@gmx.de
+ *
+ *   This program is free software: you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation, either version 3 of the License, or
+ *   (at your option) any later version.
+ *
+ *   This program is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU General Public License for more details.
+ *
+ *   You should have received a copy of the GNU General Public License
+ *   along with this program.  If not, see https://www.gnu.org/licenses/gpl-3.0.txt.
+ *
  */
 package com.github.chrbayer84.keybits;
 
-public class Bases
-{
-    private static char[] base16_alphabet = "0123456789ABCDEF".toCharArray();
+public class Bases {
+	private static char[] base16_alphabet = "0123456789ABCDEF".toCharArray();
 
-    public static byte[] base16StringToByteArray( String s )
-    {
-        int l = s.length();
-        byte[] ret = new byte[l / 2];
-        for ( int i = 0; i < l; i += 2 )
-            // get first character number in hex, divide by 16 and add second character number in hex
-            ret[i / 2] =
-                (byte) ( ( Character.digit( s.charAt( i ), 16 ) << 4 ) + Character.digit( s.charAt( i + 1 ), 16 ) );
-
-        return ret;
-    }
-
-    public static String byteArrayToBase16String( byte[] bytes )
-    {
-        char[] hex_chars = new char[2 * bytes.length];
-        for ( int j = 0; j < bytes.length; j++ )
-        {
-            int value = bytes[j] & 0xFF; // get both entries for hex-number, also necessary if byte has value 256 which
-                                         // is zero
-            hex_chars[2 * j] = Bases.base16_alphabet[value >>> 4]; // divide value by 16 to get first character, shift
-                                                                   // bits four positions
-            hex_chars[2 * j + 1] = Bases.base16_alphabet[value & 0x0F]; // get second entry for hex-number
-        }
-        return new String( hex_chars );
-    }
+	public static byte[] base16StringToByteArray(String s) {
+	    int l = s.length();
+	    byte[] ret = new byte[l/2];
+	    for (int i = 0; i < l; i += 2)
+	    	// get first character number in hex, divide by 16 and add second character number in hex
+	        ret[i/2] = (byte) ((Character.digit(s.charAt(i), 16) << 4) + Character.digit(s.charAt(i + 1), 16));
+	    
+	    return ret;
+	}
+	
+	public static String byteArrayToBase16String(byte[] bytes) {
+	    char[] hex_chars = new char[2*bytes.length];
+	    for ( int j = 0; j < bytes.length; j++ ) {
+	        int value = bytes[j] & 0xFF;								// get both entries for hex-number, also necessary if byte has value 256 which is zero
+	        hex_chars[2*j] = Bases.base16_alphabet[value >>> 4];		// divide value by 16 to get first character, shift bits four positions
+	        hex_chars[2*j + 1] = Bases.base16_alphabet[value & 0x0F];	// get second entry for hex-number
+	    }
+	    return new String(hex_chars);
+	}
 }
--- src/main/java/com/github/chrbayer84/keybits/Hashes.java
+++ src/main/java/com/github/chrbayer84/keybits/Hashes.java
@@ -1,10 +1,20 @@
 /**
- * Copyright (C) 2014 keybits@gmx.de This program is free software: you can redistribute it and/or modify it under the
- * terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the
- * License, or (at your option) any later version. This program is distributed in the hope that it will be useful, but
- * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See
- * the GNU General Public License for more details. You should have received a copy of the GNU General Public License
- * along with this program. If not, see https://www.gnu.org/licenses/gpl-3.0.txt.
+ *   Copyright (C) 2014
+ *   keybits@gmx.de
+ *
+ *   This program is free software: you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation, either version 3 of the License, or
+ *   (at your option) any later version.
+ *
+ *   This program is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU General Public License for more details.
+ *
+ *   You should have received a copy of the GNU General Public License
+ *   along with this program.  If not, see https://www.gnu.org/licenses/gpl-3.0.txt.
+ *
  */
 package com.github.chrbayer84.keybits;
 
@@ -13,65 +23,63 @@ import org.spongycastle.crypto.digests.SHA256Digest;
 import org.spongycastle.crypto.digests.SHA512Digest;
 import org.spongycastle.crypto.digests.WhirlpoolDigest;
 
-public class Hashes
-{
-    public static byte[] byteArrayToAddress( byte[] data )
-    {
-        byte[] fst = new byte[data.length + 1];
-        System.arraycopy( data, 0, fst, 1, data.length );
+public class Hashes {
+	public static byte[] byteArrayToAddress(byte[] data){
+		byte[] fst = new byte[data.length + 1];
+		System.arraycopy(data, 0, fst, 1, data.length);
 
-        byte[] snd = Hashes.toSHA256( fst );
+		byte[] snd = Hashes.toSHA256(fst);
 
-        byte[] trd = Hashes.toSHA256( snd );
-
-        byte[] bba = new byte[fst.length + 4];
-        System.arraycopy( fst, 0, bba, 0, fst.length );
-        System.arraycopy( trd, 0, bba, bba.length - 4, 4 );
-
-        return bba;
-    }
-
-    public static byte[] toRIPEMD160( byte[] in )
-    {
-        byte[] out = new byte[20];
-
-        RIPEMD160Digest digest = new RIPEMD160Digest();
-        digest.update( in, 0, in.length );
-        digest.doFinal( out, 0 );
-
-        return out;
-    }
-
-    public static byte[] toSHA256( byte[] in )
-    {
-        byte[] out = new byte[32];
-
-        SHA256Digest digest = new SHA256Digest();
-        digest.update( in, 0, in.length );
-        digest.doFinal( out, 0 );
-
-        return out;
-    }
-
-    public static byte[] toSHA512( byte[] in )
-    {
-        byte[] out = new byte[64];
-
-        SHA512Digest digest = new SHA512Digest();
-        digest.update( in, 0, in.length );
-        digest.doFinal( out, 0 );
-
-        return out;
-    }
-
-    public static byte[] toWhirlpool( byte[] in )
-    {
-        byte[] out = new byte[64];
-
-        WhirlpoolDigest digest = new WhirlpoolDigest();
-        digest.update( in, 0, in.length );
-        digest.doFinal( out, 0 );
-
-        return out;
-    }
+		byte[] trd = Hashes.toSHA256(snd);
+		
+		byte[] bba = new byte[fst.length + 4];
+		System.arraycopy(fst, 0, bba, 0, fst.length);
+		System.arraycopy(trd, 0, bba, bba.length - 4, 4);
+		
+		return bba;
+	}
+	
+	public static byte[] toRIPEMD160(byte[] in)
+	{
+	    byte[] out = new byte[20];
+	    
+	    RIPEMD160Digest digest = new RIPEMD160Digest();
+	    digest.update(in, 0, in.length);
+	    digest.doFinal(out, 0);
+	    
+	    return out;
+	}
+	
+	public static byte[] toSHA256(byte[] in)
+	{
+	    byte[] out = new byte[32];
+	    
+	    SHA256Digest digest = new SHA256Digest();
+	    digest.update(in, 0, in.length);
+	    digest.doFinal(out, 0);
+	    
+	    return out;
+	}
+	
+	public static byte[] toSHA512(byte[] in)
+	{
+	    byte[] out = new byte[64];
+	    
+	    SHA512Digest digest = new SHA512Digest();
+	    digest.update(in, 0, in.length);
+	    digest.doFinal(out, 0);
+	    
+	    return out;
+	}
+	
+	public static byte[] toWhirlpool(byte[] in)
+	{
+	    byte[] out = new byte[64];
+	    
+	    WhirlpoolDigest digest = new WhirlpoolDigest();
+	    digest.update(in, 0, in.length);
+	    digest.doFinal(out, 0);
+	    
+	    return out;
+	}
 }
